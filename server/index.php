27bdbab351dde0765df9920d6b956fd9af78c7d5
kmpoy <79004182+kmpoy@users.noreply.github.com>
2021-02-14 08:38:34 +0100

    ki

--- server/index.php
+++ server/index.php
@@ -14,55 +14,66 @@
 	$splitted_url_1 = explode('/', $url);
 	$queries = explode('?', $splitted_url_1[5]);
 	$location = $queries[0];
-
-	$table_name=$location."_COM";
-	$result = mysqli_query($connection, "show tables like".'"'.$table_name.'"');
-	if(mysqli_num_rows($result)==0){
-		$query = "CREATE TABLE ".$table_name."(
-			`id` int,
-			`username` varchar(20),
-			`comment` varchar(500),
-			`date` DATE,
-			`likes` int)";
-		mysqli_query($connection, $query);
+	$locations = [];
+	$result = mysqli_query($connection,"select location FROM covid_data");
+	while($row = mysqli_fetch_row($result)){			
+			array_push($locations, $row[0]);
 	}
-
-	if(count($queries)>1){
-		$idd = $queries[1];
-		$username = $queries[2];
-		$date=date("Y")."-".date("m")."-".date("d");
-		$state = $queries[3];
-		if(count($queries) > 4){
-			$comment = $queries[4];
-			$comment_replaced = str_replace("%20", " ", $comment);
+	$correct_input = false;
+	foreach ($locations as $value) {
+		if($value == $location)
+			$correct_input = True;
+	}
+	if($correct_input){																				
+		$table_name=$location."_COM";
+		$result = mysqli_query($connection, "show tables like".'"'.$table_name.'"');
+		if(mysqli_num_rows($result)==0){
+			$query = "CREATE TABLE ".$table_name."(
+				`id` int,
+				`username` varchar(20),
+				`comment` varchar(500),
+				`date` DATE,
+				`likes` int)";
+			mysqli_query($connection, $query);
 		}
 
-		if($state == "L"){ 	#insert comment
-			$query="update ".$table_name." SET likes = likes + 1 where id=".$idd;
-		}
+		if(count($queries)>1){
+			$idd = $queries[1];
+			$username = $queries[2];
+			$date=date("Y")."-".date("m")."-".date("d");
+			$state = $queries[3];
+			if(count($queries) > 4){
+				$comment = $queries[4];
+				$comment_replaced = str_replace("%20", " ", $comment);
+			}
 
-		elseif($state == "D"){
-			$query = "delete from ".$table_name." where id = ".$idd;
-		}
-		else{				#like given, no comment written
+			if($state == "L"){ 	#insert comment
+				$query="update ".$table_name." SET likes = likes + 1 where id=".$idd;
+			}
+
+			elseif($state == "D"){
+				$query = "delete from ".$table_name." where id = ".$idd;
+			}
+			else{				#like given, no comment written
+				
+				$query="insert into ".$table_name." values (".$idd.", 
+																	".'"'.$username.'"'.",
+																	".'"'.$comment_replaced.'"'.",
+																	".'"'.$date.'"'.",
+																	0 )";
+
+			}
 			
-			$query="insert into ".$table_name." values (".$idd.", 
-																".'"'.$username.'"'.",
-																".'"'.$comment_replaced.'"'.",
-																".'"'.$date.'"'.",
-																0 )";
-
+			mysqli_query($connection, $query);
 		}
+		$covid_info = download_info($connection,$location, "covid_data");
+		$comments = download_info($connection,$location, $table_name);
 		
-		mysqli_query($connection, $query);
+		
+		$result_array = array($covid_info,$comments);
+		$json_result = json_encode($result_array);
+		echo($json_result);
 	}
-	$covid_info = download_info($connection,$location, "covid_data");
-	$comments = download_info($connection,$location, $table_name);
-	
-	
-	$result_array = array($covid_info,$comments);
-	$json_result = json_encode($result_array);
-	echo($json_result);
 
 
 	function download_info($connection, $location, $tableName){
